aab6e142505a4aefe132fe9c9db05b8930f93dde
André Igor <andreigormenezes@gmail.com>
2023-10-02 09:58:35 -0300

    about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { DetalhesVagaComponent } from './components/pages/detalhes-vaga/detalhes-vaga.component';
 import { ContatoComponent } from './components/pages/contato/contato.component';
+import { SobreComponent } from './components/pages/sobre/sobre.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'vaga/:idVaga', component: DetalhesVagaComponent},
+  {path: 'sobre', component: SobreComponent}
   // {path: 'contato', component: ContatoComponent}
 ];
 
